de6050e8bcd6653f2387f0b4a0585aad09662d1f
root <root@ip-172-31-12-146.us-west-1.compute.internal>
2016-03-28 06:55:07 +0000

    modifying pickupdate data type to datetime

--- app/code/local/YouPick/PickupDate/sql/youpick_pickupdate_setup/mysql4-install-0.0.1.php
+++ app/code/local/YouPick/PickupDate/sql/youpick_pickupdate_setup/mysql4-install-0.0.1.php
@@ -3,24 +3,24 @@
 $installer = $this;
  
 $installer->startSetup();
- 
+
 $installer->getConnection()
     ->addColumn($installer->getTable('sales/quote_address'), 'desired_pickup_date', array(
-        'TYPE'      => Varien_Db_Ddl_Table::TYPE_DATE,
+        'TYPE'      => Varien_Db_Ddl_Table::TYPE_DATETIME,
         'NULLABLE'  => true,
         'COMMENT'   => 'Shipment Desired Pickup Date'
     ));
  
 $installer->getConnection()
     ->addColumn($installer->getTable('sales/order'), 'desired_pickup_date', array(
-        'TYPE'      => Varien_Db_Ddl_Table::TYPE_DATE,
+        'TYPE'      => Varien_Db_Ddl_Table::TYPE_DATETIME,
         'NULLABLE'  => true,
         'COMMENT'   => 'Shipment Desired Pickup Date'
     ));
  
 $installer->getConnection()
     ->addColumn($installer->getTable('sales/order_grid'), 'desired_pickup_date', array(
-        'TYPE'      => Varien_Db_Ddl_Table::TYPE_DATE,
+        'TYPE'      => Varien_Db_Ddl_Table::TYPE_DATETIME,
         'NULLABLE'  => true,
         'COMMENT'   => 'Shipment Desired Pickup Date'
     ));
